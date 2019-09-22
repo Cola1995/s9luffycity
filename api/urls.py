dd213ddd9c76610c0d07ddaa4924ee269144159c
Cola <991571566@qq.com>
2019-09-22 18:34:48 +0800

    添加登录

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url,include
 
 from api.views import course
-
+from api.views import account
 
 urlpatterns = [
 
@@ -11,4 +11,7 @@ urlpatterns = [
 
     url(r'^course/$', course.CourseView.as_view({"get":"list"})),
     url(r'^course/(?P<pk>\d+)/$', course.CourseView.as_view({"get":"retrieve"})),
+    url(r'^auth/$', account.authView.as_view()),
+
+
 ]
--- api/views/account.py
+++ api/views/account.py
@@ -1,1 +1,10 @@
-# by luffycity.com 
+# by luffycity.com
+from  rest_framework.views import APIView
+from rest_framework.response import Response
+
+class authView(APIView):
+
+    def post(self,request,*args,**kwargs):
+        print(request.data)
+
+        return Response("...")
